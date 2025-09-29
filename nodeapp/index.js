34157ca4863e20637918bc6a2c0a3e7f8f6ebad6
chamodpw2000 <mailtochamodw@gmail.com>
2025-09-29 14:17:37 +0530

    display text update

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send('{ "response": "Hello, Welcome to Kalharcodes!!!"}');
+  res.send('{ "response": "Hello, Welcome to CI/CD Docker Automation with GH Actions !!!"}');
 });
 
 //listen to port 3000 by default
